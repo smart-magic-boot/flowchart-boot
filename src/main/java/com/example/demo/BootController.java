79d6ac145b4ced8fad86c0fb8ea25baa089c853f
tank99tank <tank99tank@163.com>
2022-02-27 21:04:32 +0800

    u

--- src/main/java/com/example/demo/BootController.java
+++ src/main/java/com/example/demo/BootController.java
@@ -3,13 +3,16 @@ package com.example.demo;
 import java.io.File;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
 
 @CrossOrigin
 @RestController
@@ -20,7 +23,7 @@ public class BootController {
 	
     @PostMapping(value = "/save")
     public Object flowDataSave(@RequestBody String json) {
-    	File file = new File("src/main/resources/flow."+SDF.format(new Date())+".json");
+    	File file = new File("src/main/resources/flow.json");
     	try {
         	if(!file.exists()){
         		file.createNewFile();
@@ -34,5 +37,16 @@ public class BootController {
 			e.printStackTrace();
 		}
         return json;
+    }
+
+    @GetMapping(value = "/get")
+    public Object flowDataGet() {
+    	File file = new File("src/main/resources/flow.json");
+    	try {
+    		return JSONObject.parse(FileUtils.readFileToString(file, "UTF-8"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return null;
     }
 }
